e31f3491a19620434e102542fd6aa548a9ada9e2
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-11-05 16:44:57 -0600

    Swapped inital form for Bootstrap Form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,34 +1,54 @@
 import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import {Button} from 'react-bootstrap'
+import PropTypes from 'prop-types'
 
-export function LoginView(props) {
+import './login-view.scss'
+import { func } from 'prop-types';
+
+export function LoginView({onRouteChange}) {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 
 	const handleSubmit = e => {
 		e.preventDefault();
 		console.log(username, password);
-    props.onLoggedIn(username)
+    onRouteChange('home')
 	};
 
+  const switchToRegister = () => {
+    onRouteChange('register')
+  }
+  const {Group, Label, Control} = Form
+
 	return (
-		<form>
-			<label htmlFor='username'>Username</label>
-			<input
+		<Form>
+      <Group>
+			<Label htmlFor='username'>Username</Label>
+			<Control
 				type='text'
 				value={username}
 				name='username'
 				id='username'
 				onChange={e => setUsername(e.target.value)}
 			/>
-			<label htmlFor='password'>Password</label>
-			<input
+      </Group>
+      <Group>
+			<Label htmlFor='password'>Password</Label>
+			<Control
 				type='password'
 				value={password}
 				id='password'
         name="password"
 				onChange={e => setPassword(e.target.value)}
 			/>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-		</form>
+      <Button type="submit" onClick={handleSubmit}>Submit</Button>
+		</Group>
+    <p>Don't have an account? <Button type="button" onClick={switchToRegister}>Register</Button></p>
+    </Form>
 	);
+}
+
+LoginView.propTypes = {
+  onRouteChange: PropTypes.func.isRequired
 }
