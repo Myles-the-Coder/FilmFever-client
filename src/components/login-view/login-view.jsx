38336a76bdb8e15a4ee89831f53ae7166921989a
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-11-08 11:36:12 -0700

    Changed login to access server data

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,55 +1,70 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { Form } from 'react-bootstrap';
-import {Button} from 'react-bootstrap'
-import PropTypes from 'prop-types'
+import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-import './login-view.scss'
+import './login-view.scss';
 
-export function LoginView({onRouteChange}) {
+export function LoginView({ onRouteChange, onLoggedIn }) {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 
 	const handleSubmit = e => {
 		e.preventDefault();
-		console.log(username, password);
-    onRouteChange('home')
+		axios
+			.post('https://film-fever-api.herokuapp.com/login', {
+				Username: username,
+				Password: password,
+			})
+			.then(res => onLoggedIn(res.data))
+			.catch(err => console.log('No such user'));
+
+		onRouteChange('home');
 	};
 
-  const switchToRegister = () => {
-    onRouteChange('register')
-  }
-  const {Group, Label, Control} = Form
+	const switchToSignup = () => {
+		onRouteChange('register');
+	};
+
+	const { Group, Label, Control } = Form;
 
 	return (
 		<Form className='mt-2'>
-      <Group>
-			<Label htmlFor='username'>Username:</Label>
-			<Control
-				type='text'
-				value={username}
-				name='username'
-				id='username'
-        placeholder='Enter Username...'
-				onChange={e => setUsername(e.target.value)}
-			/>
-      </Group>
-      <Group>
-			<Label htmlFor='password'>Password:</Label>
-			<Control
-				type='password'
-				value={password}
-				id='password'
-        name="password"
-        placeholder="Enter Password..."
-				onChange={e => setPassword(e.target.value)}
-			/>
-      <Button type="submit" onClick={handleSubmit} className='m-2'>Submit</Button>
-		</Group>
-    <p>Don't have an account? <Button type="button" onClick={switchToRegister}>Register</Button></p>
-    </Form>
+			<Group controlId='formUsername'>
+				<Label>Username:</Label>
+				<Control
+					type='text'
+					value={username}
+					name='username'
+					placeholder='Enter Username...'
+					onChange={e => setUsername(e.target.value)}
+				/>
+			</Group>
+			<Group controlId='formPassword'>
+				<Label>Password:</Label>
+				<Control
+					type='password'
+					value={password}
+					name='password'
+					placeholder='Enter Password...'
+					onChange={e => setPassword(e.target.value)}
+				/>
+				<Button type='submit' onClick={handleSubmit} className='m-2'>
+					Submit
+				</Button>
+			</Group>
+			<p>
+				Don't have an account?{' '}
+				<Button type='button' onClick={switchToSignup}>
+					Sign Up
+				</Button>
+			</p>
+		</Form>
 	);
 }
 
 LoginView.propTypes = {
-  onRouteChange: PropTypes.func.isRequired
-}
+	onRouteChange: PropTypes.func.isRequired,
+	onLoggedIn: PropTypes.func.isRequired,
+};
