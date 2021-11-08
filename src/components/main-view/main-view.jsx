d03a45ced7ee6bd711c520fa8cbdc8dd167f3bc2
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-11-08 16:35:00 -0700

    Added Login in/out authentication

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router';
 import axios from 'axios';
 import Navigation from '../navigation/navigation';
 import MovieCard from '../movie-card/movie-card';
 import MovieView from '../movie-view/movie-view';
-import { LoginView } from '../login-view/login-view';
-import { RegistrationView } from '../registration-view/registration-view';
+import LoginView from '../login-view/login-view';
+import RegistrationView from '../registration-view/registration-view';
 import { Row, Col, Container } from 'react-bootstrap';
 
 import './main-view.scss';
@@ -15,40 +16,51 @@ class MainView extends React.Component {
 		this.state = {
 			movies: [],
 			selectedMovie: null,
-			isSignedIn: false,
-			user: {},
+			user: null,
 			route: 'login',
 		};
 	}
 
-	// componentDidMount() {
-	// 	axios
-	// 		.get('https://film-fever-api.herokuapp.com/movies')
-	// 		.then(res => this.setState({ movies: res.data }))
-	// 		.catch(err => console.log(err));
-	// }
+	componentDidMount = () => {
+    let accessToken = localStorage.getItem('token')
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      })
+      this.getMovies(accessToken)
+    }
+  }
 
-	setSelectedMovie(newSelectedMovie) {
+	setSelectedMovie = (newSelectedMovie) => {
 		this.setState({
 			selectedMovie: newSelectedMovie,
 		});
 	}
 
-	onLoggedIn(authData) {
-		console.log(authData);
+	onLoggedIn = (authData) => {
 		this.setState({ user: authData.user.Username });
 		localStorage.setItem('token', authData.token);
 		localStorage.setItem('user', authData.user.Username);
 
-		this.getMovies(authData);
+		this.getMovies(authData.token);
 	}
 
-	getMovies(token) {
+  onLoggedOut = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    this.setState({user: null})
+    this.onRouteChange('login')
+  }
+
+	getMovies = (token) => {
 		axios
 			.get('https://film-fever-api.herokuapp.com/movies', {
 				headers: { Authorization: `Bearer ${token}` },
 			})
-			.then(res => this.setState({ movies: res.data }))
+			.then(res => {
+        this.setState({ movies: res.data })
+        this.onRouteChange('home')
+      })
 			.catch(err => console.log(err));
 	}
 
@@ -68,7 +80,7 @@ class MainView extends React.Component {
 			return (
 				<>
 					<Navigation
-						isSignedIn={isSignedIn}
+						route={route}
 						onRouteChange={this.onRouteChange}
 					/>
 					<Container>
@@ -86,13 +98,13 @@ class MainView extends React.Component {
 				</>
 			);
 
-		if (movies.length === 0) return <div className='main-view' />;
+		// if (movies.length === 0) return <div className='main-view' />;
 
 		return (
 			<div className='main-view'>
 				<Navigation
-					isSignedIn={isSignedIn}
-					onRouteChange={this.onRouteChange}
+          onLoggedOut={this.onLoggedOut}
+          route={route}
 				/>
 				{route === 'home' ? (
 					<Container>
